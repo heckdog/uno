2558cf5b0d04d535bd4333d7dd89af3e6ba6d70f
DevHub <thatbearduse@gmail.com>
2018-12-02 19:16:33 -0500

    card generator
    shuffles cards and whatnot

--- program.py
+++ program.py
@@ -1,10 +1,14 @@
 # another quality program
+import random
 
 cards = ["1 RED"]
 
 
 def main():
     print_header()
+    deck = get_deck()
+    for card in deck:
+        print(card)
 
 
 def print_header():
@@ -20,32 +24,34 @@ def print_header():
     print("------------------------------")
 
 
-deck = []
-# The below just creates the deck.
-for color in ["RED", "GREEN", "BLUE", "YELLOW"]:
-    # Cards 0-9
-    for i in range(10):
-        deck.append(str(i) + " " + color)
-    # Another set of 1-9
-    for i in range(1,10):
-        deck.append(str(i) + " " + color)
-    # Action Cards
-    deck.append("REVERSE " + color)
-    deck.append("REVERSE " + color)
-    deck.append("SKIP " + color)
-    deck.append("SKIP " + color)
-    deck.append("DRAW 2 " + color)
-    deck.append("DRAW 2 " + color)
+def get_deck():
+    deck = []
+    # The below just creates the deck.
+    for color in ["RED", "GREEN", "BLUE", "YELLOW"]:
+        # Cards 0-9
+        for i in range(10):
+            deck.append(str(i) + " " + color)
+        # Another set of 1-9
+        for i in range(1, 10):
+            deck.append(str(i) + " " + color)
+        # Action Cards
+        deck.append("REVERSE " + color)
+        deck.append("REVERSE " + color)
+        deck.append("SKIP " + color)
+        deck.append("SKIP " + color)
+        deck.append("DRAW 2 " + color)
+        deck.append("DRAW 2 " + color)
 
-# Wild Cards
-for i in range(4):
-    deck.append("WILDCARD")
-    deck.append("WILD +4")
+    # Wild Cards
+    for i in range(4):
+        deck.append("WILDCARD")
+        deck.append("WILD +4")
 
-for card in deck:
-    print(card)
+    random.shuffle(deck)
+    return deck
 
-
+    # for card in deck:
+    # print(card)
 
 
 if __name__ == "__main__":
